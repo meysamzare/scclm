5cff3c799cfb59b81a011e147bda36b0129fe9d9
Meysam Zare <meysam.src@gmail.com>
2020-04-05 00:07:12 +0430

    - change Index Version to "2.6.4 Build 7500" - (Fix) the external url files for product view

--- src/app/shared/Auth/auth.service.ts
+++ src/app/shared/Auth/auth.service.ts
@@ -88,7 +88,7 @@ export class AuthService {
 
     DASHBOARD_VERSION = "0.9.9.8.5 Build 5420";
 
-    INDEX_VERSION = "2.6.3 Build 980";
+    INDEX_VERSION = "2.6.4 Build 7500";
 
     PARENTMOBILEAPP_VERSION = "2.9.1 Build 5800";
     TMA_VERSION = "1.8.6 Build 310";
