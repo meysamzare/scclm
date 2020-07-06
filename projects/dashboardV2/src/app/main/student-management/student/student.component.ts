43138f1e0e00552f3b50bedc1447bfbd70496b9b
Meysam Zare <meysam.src@gmail.com>
2020-07-07 00:32:49 +0430

    - fix student component style url

--- projects/dashboardV2/src/app/main/student-management/student/student.component.ts
+++ projects/dashboardV2/src/app/main/student-management/student/student.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-student',
-  templateUrl: './student.component.html',
-  styleUrls: ['./student.component.css']
+    selector: 'app-student',
+    templateUrl: './student.component.html',
+    styleUrls: ['./student.component.scss']
 })
 export class StudentComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
