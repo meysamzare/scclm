360aaaa833d6a8163236a125416e34bbccedec71
Meysam Zare <meysam.src@gmail.com>
2020-10-30 19:41:19 +0330

    - Add route for student edit and list

--- projects/dashboardV2/src/app/main/student-management/student/student-routing.module.ts
+++ projects/dashboardV2/src/app/main/student-management/student/student-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IStudent } from 'src/app/Dashboard/student/student';
+import { DataEditResolverService } from '../../../shared/reusable-component/data/data-edit/data-edit-resolver.service';
+import { StudentEditComponent } from './student-edit/student-edit.component';
+import { StudentListComponent } from './student-list/student-list.component';
 
 import { StudentComponent } from './student.component';
 
 const routes: Routes = [
-    { path: '', component: StudentComponent }
+    { path: '', component: StudentComponent },
+    {
+        path: "list",
+        component: StudentListComponent
+    },
+    {
+        path: "edit/:id",
+        component: StudentEditComponent,
+        data: {
+            newData: new IStudent,
+            apiUrl: "Student"
+        },
+        resolve: {
+            data: DataEditResolverService
+        }
+    }
 ];
 
 @NgModule({
